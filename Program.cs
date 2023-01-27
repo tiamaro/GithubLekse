2ab1822e3f09b9a882bbbf1da1d5803cfac27436
tiamaro <tina.olsen@gokstadakademiet.no>
2023-01-27 14:27:20 +0100

    gren2: legger til input

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Jeg lærer å bruke git og github!");
+
+Console.Write("Hei, hva heter du? ");
+var name = Console.ReadLine();
+Console.WriteLine($"Hei på deg, {name}!");
