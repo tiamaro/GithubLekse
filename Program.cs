8dc71c176ba59be8b2d4b1a3fa668e5af618d240
tiamaro <tina.olsen@gokstadakademiet.no>
2023-01-27 14:37:15 +0100

    gren3: lager for-løkke med tall fra 1-10

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Jeg lærer å bruke git og github!");
+
+Console.WriteLine("\nSkriver ut tallrekke fra 1 til 10");
+for (int i = 1; i < 11; i++)
+{
+    Console.Write(" " + i);
+}
+Console.ReadKey();
